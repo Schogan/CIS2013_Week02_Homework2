0bcb5484fcf6aadb4dd7eecc90bd657cc37f7dbe
Schogan <lrschanbacher@gmail.com>
2018-01-31 20:25:28 -0600

    removed int i

--- CIS2013_Week02_Homework2.cpp
+++ CIS2013_Week02_Homework2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-	int i = 1;
+	
 	// for loop starts i at 99 and subtracts 1 every time it loops until it gets to 1
 	for (i=99;i>=1;i--){
 		cout << i << " bottles of beer on the wall, " << i <<
